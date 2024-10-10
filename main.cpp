f31cce92c8430b7abb93572f1a4fd42db3e76e1f
ReBarbaro <francescovito.dipalo@marconibari.edu.it>
2024-10-10 08:21:52 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,31 @@
 using namespace std;
 
 int main() {
+    int debito,promosso,i,N;
+    float somma,voto,media;
     string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    cout<<"quanti studenti ci sono?"<<endl;
+    cin>>N;
+    i=0;
+    debito=0;
+    promosso=0;
+    somma=0;
+    while(i<N){
+        cout<<"come ti chiami?"<<endl;
+        cin>>nome;
+        cout<<"quale è il tuo nome?"<<endl;
+        cin>>voto;
+        if(voto>=6){
+            cout<<"sei stato promosso"<<endl;
+            promosso=promosso+1;
+            }else{
+                cout<<"sei stato bocciato"<<endl;
+                debito=debito+1;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+somma=somma+1;
+i=i+1;
+}
+cout<<"il numero dei debiti è"<<debito<<"il numero dei promossi è"<<promosso<<endl;
+media=somma/N;
+cout<<"la media della classe è"<<media<<endl;
+}
